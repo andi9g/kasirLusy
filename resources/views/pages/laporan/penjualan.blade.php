d954c6bfec3dd7b0987519d235da7caae7f20cea
andi9g <andirizky.bayuputra@gmail.com>
2024-06-12 20:54:17 +0700

    oke

--- resources/views/pages/laporan/penjualan.blade.php
+++ resources/views/pages/laporan/penjualan.blade.php
@@ -51,9 +51,6 @@
 
     <table width="100%" style="border-bottom: 2px double black">
         <tr>
-            <td width="100px" valign="top" >
-                <img src="{{ url('gambar/logo', ['logo.png']) }}" width="100%" alt="">
-            </td>
             <td valign="middle">
                 <h1 style="text-transform: uppercase">Vansesco Boutique</h1>
                 <h3>Jl. Teladan No.02 Tanjungpinang Barat</h3>
